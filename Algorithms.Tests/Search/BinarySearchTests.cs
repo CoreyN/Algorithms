7bd5be1e1fc1db6707aae17b9d0bffb29ddc4ebb
Corey Napier <coreyrnapier@gmail.com>
2015-10-17 12:01:35 -0400

    Added BinarySearch.

---
+++ Algorithms.Tests/Search/BinarySearchTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.Search;
+
+namespace Algorithms.Tests.Search
+{
+    [TestClass]
+    public class BinarySearchTests
+    {
+        IList<string> _haystack;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _haystack = new string[] { "a", "b", "c", "d", "e" };
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Search_NeedleNull_ThrowsException()
+        {
+            BinarySearch.Search(null, _haystack);
+        }
+
+        [TestMethod]
+        public void Search_ElementInArray_ReturnsIndex()
+        {
+            Assert.AreEqual(0, BinarySearch.Search("a", _haystack));
+            Assert.AreEqual(1, BinarySearch.Search("b", _haystack));
+            Assert.AreEqual(2, BinarySearch.Search("c", _haystack));
+            Assert.AreEqual(3, BinarySearch.Search("d", _haystack));
+            Assert.AreEqual(4, BinarySearch.Search("e", _haystack));
+        }
+
+        [TestMethod]
+        public void Search_ElementNotInArray_ReturnsNull()
+        {
+            Assert.AreEqual(null, BinarySearch.Search("f", _haystack));
+        }
+    }
+}
---
+++ Algorithms/Search/BinarySearch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Search
+{
+    public static class BinarySearch
+    {
+        /* Precondition: the list to be searched must be sortred.
+         * Worst case run time: O(lg n)
+         */
+        public static int? Search<T>(T needle, IList<T> haystack) where T : IComparable<T>
+        {
+            if (needle == null)
+                throw new ArgumentNullException("needle");
+
+            int low = 0;
+            int high = haystack.Count - 1;
+
+            //Loop invariant: if the needle is in the haystack, then its index
+            //in haystack is in the closed interval [low, high].
+            while(high >= low)
+            {
+                int middle = GetMiddleIndex(low, high);
+
+                if(haystack[middle].CompareTo(needle) < 0)
+                    low = middle + 1;
+                else if (haystack[middle].CompareTo(needle) > 0)
+                    high = middle - 1;
+                else
+                    return middle;
+            }
+
+            return null;
+        }
+
+        private static int GetMiddleIndex(int low, int high)
+        {
+            //Using this formula to calculate the middle index avoids a possible integer
+            //overflow on very large arrays.
+            return (high - low) / 2 + low;
+        }
+    }
+}
