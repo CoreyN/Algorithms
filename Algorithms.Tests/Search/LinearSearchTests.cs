9c3bc82474918a9499eecc28a8ef92f35dcdca84
Corey Napier <coreyrnapier@gmail.com>
2015-10-17 01:15:38 -0400

    Initial commit.

---
+++ Algorithms.Tests/Search/LinearSearchTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.Search;
+
+namespace Algorithms.Tests.Search
+{
+    [TestClass]
+    public class LinearSearchTests
+    {
+        IList<string> _haystack;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _haystack = new string[] { "a", "b", "c" };
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Search_NeedleNull_ThrowsException()
+        {
+            LinearSearch.Search<string>(null, _haystack);
+        }
+
+        [TestMethod]
+        public void Search_ElementInArray_ReturnsIndex()
+        {
+            Assert.AreEqual(0, LinearSearch.Search<string>("a", _haystack));
+            Assert.AreEqual(1, LinearSearch.Search<string>("b", _haystack));
+            Assert.AreEqual(2, LinearSearch.Search<string>("c", _haystack));
+        }
+
+        [TestMethod]
+        public void Search_ElementNotInArray_ReturnsNull()
+        {
+            Assert.AreEqual(null, LinearSearch.Search<string>("d", _haystack));
+        }
+    }
+}
---
+++ Algorithms/Search/LinearSearch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Search
+{
+    public static class LinearSearch
+    {
+        public static int? Search<T>(T needle, IList<T> haystack) where T : IEquatable<T>
+        {
+            if (needle == null)
+                throw new ArgumentNullException("needle");
+
+            for(int i = 0; i < haystack.Count; i++)
+            {
+                if (needle.Equals(haystack[i]))
+                    return i;
+            }
+
+            return null;
+        }
+    }
+}
