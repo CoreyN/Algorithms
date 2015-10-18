65ce760293de4dfee2dea5294f1260e8066275a0
Corey Napier <coreyrnapier@gmail.com>
2015-10-18 14:43:34 -0400

    Added Queue.

---
+++ Algorithms.Tests/DataStructures/QueueTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.DataStructures;
+
+namespace Algorithms.Tests.DataStructures
+{
+    [TestClass]
+    public class QueueTests
+    {
+        Queue<int> _queue;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _queue = new Queue<int>();
+        }
+
+        [TestMethod]
+        public void EmptyQueue_CountWorks()
+        {
+            Assert.AreEqual(0, _queue.Count);
+        }
+
+        [TestMethod]
+        public void EmptyQueue_EnqueueElements_CountWorks()
+        {
+            _queue.Enqueue(1);
+            Assert.AreEqual(1, _queue.Count);
+
+            _queue.Enqueue(1);
+            Assert.AreEqual(2, _queue.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void EmptyQueue_Peek_ThrowsInvalidOperationException()
+        {
+            _queue.Peek();
+        }
+
+        [TestMethod]
+        public void Queue_Peek_Works()
+        {
+            _queue.Enqueue(1);
+            Assert.AreEqual(1, _queue.Peek());
+            Assert.AreEqual(1, _queue.Count);
+
+            _queue.Enqueue(2);
+            Assert.AreEqual(1, _queue.Peek());
+            Assert.AreEqual(2, _queue.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void EmptyQueue_Dequeue_ThrowsInvalidOperationException()
+        {
+            _queue.Dequeue();
+        }
+
+        [TestMethod]
+        public void QueueWithElements_Dequeue_Works()
+        {
+            EnqueueElements(new int[] { 1, 2, 3 });
+
+            Assert.AreEqual(1, _queue.Dequeue());
+            Assert.AreEqual(2, _queue.Dequeue());
+            Assert.AreEqual(3, _queue.Dequeue());
+        }
+
+        [TestMethod]
+        public void StackWithElements_PoppingElements_DecreasesCount()
+        {
+            EnqueueElements(new int[] { 1, 2, 3 });
+
+            _queue.Dequeue();
+            Assert.AreEqual(2, _queue.Count);
+
+            _queue.Dequeue();
+            Assert.AreEqual(1, _queue.Count);
+
+            _queue.Dequeue();
+            Assert.AreEqual(0, _queue.Count);
+        }
+
+        private void EnqueueElements(int[] elements)
+        {
+            elements.ToList().ForEach(e => _queue.Enqueue(e));
+        }
+    }
+}
---
+++ Algorithms/DataStructures/Queue.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Algorithms.DataStructures
+{
+    public class Queue<T>
+    {
+        Node _head;
+        Node _tail;
+        int _count;
+
+        public int Count { get { return _count; } }
+
+        public Queue()
+        {
+        }
+
+        public void Enqueue(T item)
+        {
+            var newNode = new Node { Item = item };
+
+            if (Count == 0)
+            {
+                _head = newNode;
+                _tail = newNode;
+            }
+            else
+            {
+                _tail.Next = newNode;
+                _tail = newNode;
+            }
+
+            _count++;
+        }
+
+        public T Dequeue()
+        {
+            ThrowInvalidOperationExceptionIfEmpty("Dequeue");
+            T item = _head.Item;
+            _head = _head.Next;
+            _count--;
+            return item;
+        }
+
+        public T Peek()
+        {
+            ThrowInvalidOperationExceptionIfEmpty("Peek");
+            return _head.Item;
+        }
+
+        private void ThrowInvalidOperationExceptionIfEmpty(string methodName)
+        {
+            string errorMessage = $"Cannot {methodName}() when Count == 0.";
+
+            if (Count == 0)
+                throw new InvalidOperationException(errorMessage);
+        }
+
+        class Node
+        {
+            public Node Next { get; set; }
+            public T Item { get; set; }
+        }
+    }
+}
