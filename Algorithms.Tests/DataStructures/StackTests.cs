1d0f021cb50c31295d7a383dc5d2d2214a424654
Corey Napier <coreyrnapier@gmail.com>
2015-10-18 12:34:43 -0400

    Added Stack.

---
+++ Algorithms.Tests/DataStructures/StackTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.DataStructures;
+
+namespace Algorithms.Tests.DataStructures
+{
+    [TestClass]
+    public class StackTests
+    {
+        Stack<int> _stack;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _stack = new Stack<int>();
+        }
+
+        [TestMethod]
+        public void EmptyStack_CountWorks()
+        {
+            Assert.AreEqual(0, _stack.Count);
+        }
+
+        [TestMethod]
+        public void EmptyStack_PushElements_CountWorks()
+        {
+            _stack.Push(1);
+            Assert.AreEqual(1, _stack.Count);
+
+            _stack.Push(1);
+            Assert.AreEqual(2, _stack.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void EmptyStack_Peek_ThrowsInvalidOperationException()
+        {
+            _stack.Peek();
+        }
+
+        [TestMethod]
+        public void StackWithElements_Peek_Works()
+        {
+            _stack.Push(1);
+            Assert.AreEqual(1, _stack.Peek());
+
+            _stack.Push(2);
+            Assert.AreEqual(2, _stack.Peek());
+        }
+
+        [TestMethod]
+        public void StackWithElements_Pop_Works()
+        {
+            PushElements(new int[] { 1, 2, 3 });
+
+            Assert.AreEqual(3, _stack.Pop());
+            Assert.AreEqual(2, _stack.Pop());
+            Assert.AreEqual(1, _stack.Pop());
+        }
+
+        [TestMethod]
+        public void StackWithElements_PoppingElements_DecreasesCount()
+        {
+            PushElements(new int[] { 1, 2, 3 });
+
+            _stack.Pop();
+            Assert.AreEqual(2, _stack.Count);
+
+            _stack.Pop();
+            Assert.AreEqual(1, _stack.Count);
+
+            _stack.Pop();
+            Assert.AreEqual(0, _stack.Count);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void EmptyStack_Pop_ThrowsInvalidOperationException()
+        {
+            _stack.Pop();
+        }
+
+        private void PushElements(int[] elements)
+        {
+            foreach (int n in elements)
+                _stack.Push(n);
+        }
+    }
+}
---
+++ Algorithms/DataStructures/Stack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Algorithms.DataStructures
+{
+    public class Stack<T>
+    {
+        DynamicArray<T> _array;
+
+        public int Count { get { return _array.Count; } }
+
+        public Stack()
+        {
+            _array = new DynamicArray<T>();
+        }
+
+        public void Push(T element)
+        {
+            _array.Append(element);
+        }
+
+        public T Pop()
+        {
+            ThrowInvalidOperationExceptionIfEmpty("Pop");
+            return _array.Pop();
+        }
+
+        public T Peek()
+        {
+            ThrowInvalidOperationExceptionIfEmpty("Peek");
+
+            return _array[Count - 1];
+        }
+
+        private void ThrowInvalidOperationExceptionIfEmpty(string methodName)
+        {
+            string errorMessage = $"Cannot {methodName}() when Count == 0.";
+
+            if (Count == 0)
+                throw new InvalidOperationException(errorMessage);
+        }
+    }
+}
