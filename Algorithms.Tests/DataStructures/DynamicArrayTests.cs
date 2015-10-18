f950f3f88f5c5b3215ff259c6d7f320ba521bfd8
Corey Napier <coreyrnapier@gmail.com>
2015-10-18 11:51:28 -0400

    Added DynamicArray.

---
+++ Algorithms.Tests/DataStructures/DynamicArrayTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.DataStructures;
+
+namespace Algorithms.Tests.DataStructures
+{
+    [TestClass]
+    public class DynamicArrayTests
+    {
+        DynamicArray<int> _dynamicArray;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _dynamicArray = new DynamicArray<int>();
+        }
+
+        [TestMethod]
+        public void DynamicArray_AddItem_CanIndexIt()
+        {
+            _dynamicArray.Append(1);
+                
+            Assert.AreEqual(1, _dynamicArray[0]);
+        }
+
+        [TestMethod]
+        public void DynamicArray_AddItems_CanIndexThem()
+        {
+            _dynamicArray.Append(1);
+            _dynamicArray.Append(2);
+            _dynamicArray.Append(3);
+            _dynamicArray.Append(4);
+
+            Assert.AreEqual(1, _dynamicArray[0]);
+            Assert.AreEqual(2, _dynamicArray[1]);
+            Assert.AreEqual(3, _dynamicArray[2]);
+            Assert.AreEqual(4, _dynamicArray[3]);
+        }
+
+        [TestMethod]
+        public void DynamicArray_SetByIndex_Works()
+        {
+            _dynamicArray.Append(1);
+            _dynamicArray.Append(2);
+
+            _dynamicArray[0] = 11;
+            _dynamicArray[1] = 12;
+
+            Assert.AreEqual(11, _dynamicArray[0]);
+            Assert.AreEqual(12, _dynamicArray[1]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void DynamicArray_SetByIndexOutOfRange_ThrowsArgumentOutOfRangeException()
+        {
+            _dynamicArray[0] = 2;
+
+            Assert.AreEqual(2, _dynamicArray[0]);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void DynamicArray_Pop_ThrowsIfEmpty()
+        {
+            _dynamicArray.Pop();
+        }
+
+        [TestMethod]
+        public void DynamicArray_Pop_ReturnsCorrectItem()
+        {
+            _dynamicArray.Append(1);
+            _dynamicArray.Append(2);
+            _dynamicArray.Append(3);
+
+            Assert.AreEqual(3, _dynamicArray.Pop());
+            Assert.AreEqual(2, _dynamicArray.Pop());
+            Assert.AreEqual(1, _dynamicArray.Pop());
+        }
+
+        [TestMethod]
+        public void DynamicArray_Pop_DecrementsCount()
+        {
+            _dynamicArray.Append(1);
+            _dynamicArray.Append(2);
+            _dynamicArray.Append(3);
+
+            _dynamicArray.Pop();
+            Assert.AreEqual(2, _dynamicArray.Count);
+
+            _dynamicArray.Pop();
+            Assert.AreEqual(1, _dynamicArray.Count);
+
+            _dynamicArray.Pop();
+            Assert.AreEqual(0, _dynamicArray.Count);
+        }
+
+        [TestMethod]
+        public void DynamicArray_ManyPops_DecreaseBufferSize()
+        {
+            for (int i = 0; i < 20; i++)
+                _dynamicArray.Append(i);
+
+            for (int i = 0; i < 20; i++)
+                _dynamicArray.Pop();
+
+            Assert.IsTrue(_dynamicArray.BufferSize <= 4);
+        }
+    }
+}
---
+++ Algorithms/DataStructures/DynamicArray.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Algorithms.DataStructures
+{
+    public class DynamicArray<T>
+    {
+        T[] _items;
+        int _count;
+
+        public DynamicArray()
+        {
+            _items = new T[1];
+            _count = 0;
+        }
+
+        public void Append(T item)
+        {
+            IncreaseSizeIfFull();
+            InsertItemAtEnd(item);
+        }
+
+        public T Pop()
+        {
+            if (Count == 0)
+                throw new InvalidOperationException("Tried to Pop() when Count == 0.");
+
+            T item = PopItem();
+            DecreaseBufferSizeIfItemsTooBig();
+            return item;
+        }
+
+        public T this[int key]
+        {
+            get
+            {
+                return _items[key];
+            }
+            set
+            {
+                if (key >= _count)
+                    throw new ArgumentOutOfRangeException();
+
+                _items[key] = value;
+            }
+        }
+
+        public int Count
+        {
+            get { return _count; }
+        }
+
+        public int BufferSize
+        {
+            get { return _items.Length; }
+        }
+
+        private void IncreaseSizeIfFull()
+        {
+            if (_count == _items.Length)
+                ResetArraySize();
+        }
+
+        private void ResetArraySize()
+        {
+            int newSize = _count * 2 > 0 ? _count * 2 : 1; 
+
+            T[] newArray = new T[newSize];
+            Array.Copy(_items, newArray, _count);
+            _items = newArray;
+        }
+
+        private void InsertItemAtEnd(T item)
+        {
+            _items[_count] = item;
+            _count++;
+        }
+
+        private T PopItem()
+        {
+            T item = _items[_count - 1];
+            _count--;
+
+            DecreaseBufferSizeIfItemsTooBig();
+
+            return item;
+        }
+
+        private void DecreaseBufferSizeIfItemsTooBig()
+        {
+            if (BufferSize > 4 * Count)
+                ResetArraySize();
+        }
+    }
+}
