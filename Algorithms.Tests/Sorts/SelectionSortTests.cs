c7621f207f828b99dab8ccbee96b07b460aa7c45
Corey Napier <coreyrnapier@gmail.com>
2015-10-19 20:19:01 -0400

    Added SelectionSort.

---
+++ Algorithms.Tests/Sorts/SelectionSortTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.Sorts;
+
+namespace Algorithms.Tests.Sorts
+{
+    [TestClass]
+    public class SelectionSortTests
+    {
+        int[] _array;
+        int[] _actualSortedArray;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _array = new int[] { 1, -2, 5, 12, -99};
+            _actualSortedArray = new int[] { -99, -2, 1, 5, 12 };
+        }
+
+        [TestMethod]
+        public void TestSelectionSort()
+        {
+            var sortedA = GetSortedArray(_array);
+            AssertArraysEqual(sortedA, _actualSortedArray);
+        }
+
+        private int[] GetSortedArray(int[] array)
+        {
+            var newArray = new int[array.Length];
+            Array.Copy(array, newArray, array.Length);
+            SelectionSort.Sort(newArray);
+            return newArray;
+        }
+
+        private void AssertArraysEqual(int[] a, int[] b)
+        {
+            Assert.IsTrue(a.SequenceEqual(b), "Arrays are not equal.");
+        }
+    }
+}
---
+++ Algorithms/Sorts/SelectionSort.cs
@@ -0,0 +1,36 @@
+﻿namespace Algorithms.Sorts
+{
+    public static class SelectionSort
+    {
+        public static void Sort(int[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                int indexOfMin = FindIndexOfMinimumElementInTail(array, i);
+                Swap(array, i, indexOfMin);
+            }
+        }
+
+        private static int FindIndexOfMinimumElementInTail(int[] array, int startIndex)
+        {
+            int indexOfMin = startIndex;
+
+            for (int j = startIndex; j < array.Length; j++)
+            {
+                if (array[j] < array[indexOfMin])
+                {
+                    indexOfMin = j;
+                }
+            }
+
+            return indexOfMin;
+        }
+
+        private static void Swap(int[] array, int a, int b)
+        {
+            int temp = array[a];
+            array[a] = array[b];
+            array[b] = temp;
+        }
+    }
+}
